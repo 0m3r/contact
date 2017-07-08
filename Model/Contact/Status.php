588dd370fc4a7f1d37e5bf5311974f6c8f01983c
Alexander Kras'ko <0m3r.mail@gmail.com>
2017-07-08 14:11:45 +0300

    add status interface

---
+++ Model/Contact/Status.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * @category  Omer
+ * @package   Omer\Contact
+ * @author    Alexander Kras'ko <0m3r.mail@gmail.com>
+ * @copyright 2017
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Omer\Contact\Model\Contact;
+
+use Magento\Framework\Data\OptionSourceInterface;
+use Omer\Contact\Api\Data\ContactInterface;
+
+/**
+ *
+ * @category  Omer
+ * @package   Omer\Contact
+ * @author    Alexander Kras'ko <0m3r.mail@gmail.com>
+ */
+class Status implements OptionSourceInterface
+{
+    /**
+     * Retrieve option array
+     *
+     * @return string[]
+     */
+    public static function getOptionArray()
+    {
+        return [
+            ContactInterface::STATUS_PENDING  => __('Pending'),
+            ContactInterface::STATUS_APPROVED => __('Approved'),
+            ContactInterface::STATUS_CLOSE    => __('Close')
+        ];
+    }
+
+    /**
+     * Get options as array
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return $this->getAllOptions();
+    }
+
+    /**
+     * Retrieve option array with empty value
+     *
+     * @return string[]
+     */
+    public function getAllOptions()
+    {
+        $result = [];
+
+        foreach (self::getOptionArray() as $index => $value) {
+            $result[] = ['value' => $index, 'label' => $value];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Retrieve option text by option value
+     *
+     * @param string $optionId
+     * @return string
+     */
+    public function getOptionText($optionId)
+    {
+        $options = self::getOptionArray();
+
+        return isset($options[$optionId]) ? $options[$optionId] : null;
+    }
+}
